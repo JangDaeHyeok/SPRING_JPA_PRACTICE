d68e1568b7f065a1553c9b9a25ade1817469afb3
JangDaeHyeok <jangdaehyeog@gmail.com>
2022-10-18 21:10:51 +0900

    jpa practice model4 테스트 코드 추가

---
+++ jpaTest/src/test/java/com/jdh/jpaTest/test/Model_4.java
@@ -0,0 +1,68 @@
+package com.jdh.jpaTest.test;
+
+import com.jdh.jpaTest.model.entity.Album;
+import com.jdh.jpaTest.model.entity.Book;
+import com.jdh.jpaTest.model.entity.Member;
+import com.jdh.jpaTest.model.entity.Movie;
+import com.jdh.jpaTest.model.repository.ItemRepository;
+import com.jdh.jpaTest.model.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+@Slf4j
+public class Model_4 {
+    @Autowired ItemRepository itemRepository;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("아이템_SINGLE_TABLE_테스트")
+    void 아이템_SINGLE_TABLE_테스트() {
+        Album album = new Album();
+        album.setName("테스트 앨범");
+        album.setPrice(10000);
+        album.setArtist("테스트 아티스트");
+        album.setEtc("테스트 기타");
+        itemRepository.save(album);
+        assertThat(album.getArtist()).isEqualTo("테스트 아티스트");
+
+        Book book = new Book();
+        book.setName("테스트 책");
+        book.setPrice(30000);
+        book.setAuthor("테스트 지은이");
+        book.setIsbn("테스트 isbn");
+        itemRepository.save(book);
+        assertThat(book.getAuthor()).isEqualTo("테스트 지은이");
+
+        Movie movie = new Movie();
+        movie.setName("테스트 영화");
+        movie.setPrice(7000);
+        movie.setDirector("테스트 감독");
+        movie.setActor("테스트 배우");
+        itemRepository.save(movie);
+        assertThat(movie.getDirector()).isEqualTo("테스트 감독");
+    }
+
+    // 멤버 삽입, 수정 시 date 체크
+    @Test
+    void member_MappedSuperclass_test() throws InterruptedException {
+        Member member = new Member();
+        member.setName("장대혁");
+        member = memberRepository.saveAndFlush(member);
+
+        Thread.sleep(2000);
+
+        member.setName("최미애");
+        member = memberRepository.saveAndFlush(member);
+    }
+}
