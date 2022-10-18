f33ce71609c1389519edcbd861866b0368b2db73
JangDaeHyeok <jangdaehyeog@gmail.com>
2022-10-18 21:09:51 +0900

    base entity 등록일자, 수정일자 설정

--- jpaTest/src/main/java/com/jdh/jpaTest/model/entity/BaseEntity.java
+++ jpaTest/src/main/java/com/jdh/jpaTest/model/entity/BaseEntity.java
@@ -1,13 +1,24 @@
 package com.jdh.jpaTest.model.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
-@Data
+@Getter
 @MappedSuperclass
 public class BaseEntity {
-    private Date regDt;
-    private Date modDt;
+    @CreationTimestamp
+    @Column(name = "reg_dt")
+    private Date regDt = null;
+
+    @UpdateTimestamp
+    @Column(name = "mod_dt", insertable = false)
+    private Date modDt = null;
 }
